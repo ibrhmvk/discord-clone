b1bf52fdac72e6171ae26f2ebecf4f75cc3d762e
Ibrahim V K <89912630+ibrhmvk@users.noreply.github.com>
2022-09-21 15:51:13 +0530

    landing page

---
+++ src/components/Landing-page.js
@@ -0,0 +1,13 @@
+import NavBar from "./Nav-bar";
+import Spotlight from "./Spotlight";
+
+function LandingPage() {
+  return (
+    <div className="bg-[#404eed] min-h-screen">
+      <NavBar />
+      <Spotlight />
+    </div>
+  );
+}
+
+export default LandingPage;
